c44c907b1df4f2eb65e63e6b729da3f0408ee8b4
Vikas Bahuguna <vikasedu10@gmail.com>
2022-06-17 01:21:58 +0530

    Add Room, Message models & register

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from chat.models import Room, Message
 
-# Register your models here.
+admin.site.register((Room, Message))
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
-# Create your models here.
+from datetime import datetime
+from django.contrib.auth.models import User
+class Room(models.Model):
+    name = models.CharField(max_length=1000)
+    # username = 
+class Message(models.Model):
+    value = models.CharField(max_length=100000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey('Room', on_delete=models.CASCADE)
